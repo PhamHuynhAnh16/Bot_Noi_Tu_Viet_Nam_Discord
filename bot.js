5c768d5c49c45c9bdef52eecbb6e217c351965b9
AnhP <157474368+PhamHuynhAnh16@users.noreply.github.com>
2024-08-30 12:33:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ client.on('messageCreate', async message => {
 
     if (message.content.toLowerCase() === '!reset')
     {
-        if (process.env.GIOI_HAN_RESET && message.user.id === process.env.AUTHOR.split(",")) return message.reply("Chỉ có Author mới có thể thêm từ");
+        if (process.env.GIOI_HAN_RESET && message.user.id === process.env.NGUOI_CO_THE_RESET.split(",")) return message.reply("Chỉ có Author mới có thể thêm từ");
 
         cụm_từ_trước_đó = '';
         người_dùng_trước_đó = '';
